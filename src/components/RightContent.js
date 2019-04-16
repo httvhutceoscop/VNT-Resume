8c0d3326360178d88462134ef4d29de4ebe2ea13
VietNT <viet.nguyen@fujitechjsc.com>
2019-04-16 17:50:20 +0700

    update experience

--- src/components/RightContent.js
+++ src/components/RightContent.js
@@ -6,7 +6,7 @@ class RightContent extends Component {
         <div className="w3-twothird">
             <div className="w3-container w3-card w3-white w3-margin-bottom">
                 <h2 className="w3-text-grey w3-padding-16">
-                    <i className="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>
+                    <i className="fa fa-user fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>
                     Introduction
                 </h2>
                 <div className="w3-container">
@@ -25,21 +25,97 @@ class RightContent extends Component {
                     Work Experience
                 </h2>
                 <div className="w3-container">
-                    <h5 className="w3-opacity"><strong>Front End Developer / w3schools.com</strong></h5>
+                    <h5 className="w3-opacity"><strong>FujitechjJSC</strong></h5>
                     <h6 className="w3-text-teal">
                         <i className="fa fa-calendar fa-fw w3-margin-right"></i>
-                        {/*Jan 2015 - <span className="w3-tag w3-teal w3-round">Current</span>*/}
-                        2008 - 2013
+                        March 2017 - <span className="w3-tag w3-teal w3-round">Current</span>
                     </h6>
-                    <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+                    <p>
+                        <ul>
+                            <li>Programming website using PHP and Yii2 framework, Laravel, AngularJS.</li>
+                            <li>Server administration, installing and configuring Linux server on  Centos with webservices such as: Apache, Nginx, PHP và MySQL. Almost using Docker.</li>
+                            <li>Deploy, release products by using Git.</li>
+                        </ul>
+                    </p>
+                    <h6>Completed Major Projects</h6>
+                    <ul>
+                        <li>Treco: Building a staff evaluation website, manage surveys. https://treco.achievement.co.jp</li>
+                        <li>Acrospera: Building websites to introduce businesses, restaurants. https://www.suminozuimade.com/, https://www.ran-hongkong.com/, ...</li>
+                        <li>Winners: Developing features to search jobs and search areas.</li>
+                        <li>Building CRM website for student status analysis and reporting.</li>
+                        <li>Website buying book online: http://motsachmogu.vn/</li>
+                        <li>Website introducing game shinchan: http://shinchan.vn/</li>
+                        <li>Website introducing company: http://zodia-q.com/</li>
+                        <li>Website booking event: https://narhanoiforum.com/</li>
+                    </ul>
                     <hr />
                 </div>
                 <div className="w3-container">
-                    <h5 className="w3-opacity"><strong>Web Developer / something.com</strong></h5>
+                    <h5 className="w3-opacity"><strong>Miyatsu Vietnam Co., LTD</strong></h5>
                     <h6 className="w3-text-teal">
-                        <i className="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014
+                        <i className="fa fa-calendar fa-fw w3-margin-right"></i>
+                        <strong>18 months</strong> from September 2015 to March 2017
                     </h6>
-                    <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+                    <p>
+                        <ul>
+                            <li>Programming website using PHP and Yii2 framework.</li>
+                            <li>Programming website, SOAP API, Amedus API using MVC .NET.</li>
+                            <li>Programming software mathematics using javascript, SVG and C++.</li>
+                        </ul>
+                    </p>
+                    <h6>Completed Major Projects</h6>
+                    <ul>
+                        <li><strong>Tripanzee</strong>: Website for booking hotel, travelling, airport transfer and conference. Write API for booking hotel, hotel cancellation...</li>
+                        <li><strong>Enginee</strong>: Making the software mathematics for pupil in Japan.</li>
+                        <li><strong>Java to HTML</strong>: Converting the software from Java to Javascript, HTML and SVG.</li>
+                    </ul>
+                    <hr />
+                </div>
+                <div className="w3-container">
+                    <h5 className="w3-opacity"><strong>Star Media Joint Stock Company (StarCom JSC)</strong></h5>
+                    <h6 className="w3-text-teal">
+                        <i className="fa fa-calendar fa-fw w3-margin-right"></i>
+                        <strong>02 years</strong> from July 2013 to August 2015
+                    </h6>
+                    <p>
+                        <ul>
+                            <li>Programming website using PHP</li>
+                            <li>Programming Standard SEO website for the Company basing on cms Drupal, Wordpress.</li>
+                            <li>Administration of and building website system of the Company.</li>
+                            <li>Server administration, installing and configuring Linux server on  Centos with webservices such as: Apache, Nginx, PHP và MySQL.</li>
+                        </ul>
+                    </p>
+                    <h6>Completed Major Projects</h6>
+                    <ul>
+                        <li>Website for watching free movies: http://starmovies.mobi, http://phimhayvai.com</li>
+                        <li>Website for game news and allowing to download games for free: http://stargame.vn</li>
+                        <li>Website introducing game programming department of the company: http://starmobile.vn</li>
+                        <li>Wapsite providing lottery results service: http://xoso.homnay24h.com, http://soicaumb.net</li>
+                    </ul>
+                    <br />
+                </div>
+            </div>
+
+            <div className="w3-container w3-card w3-white">
+                <h2 className="w3-text-grey w3-padding-16">
+                    <i className="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>
+                    Education
+                </h2>
+                <div className="w3-container">
+                    <h5 className="w3-opacity"><strong>Mina Japanese Center</strong></h5>
+                    <h6 className="w3-text-teal">
+                        <i className="fa fa-calendar fa-fw w3-margin-right"></i>
+                        06/2018 - 09/2018
+                    </h6>
+                    <p><em>N4 - Japanese Course</em></p>
+                    <hr />
+                </div>
+                <div className="w3-container">
+                    <h5 className="w3-opacity"><strong>SEO Pro Internet Marketing 41 - Litado</strong></h5>
+                    <h6 className="w3-text-teal">
+                        <i className="fa fa-calendar fa-fw w3-margin-right"></i>08/2014 - 10/2014
+                    </h6>
+                    <p><em>SEO Website Training Course</em></p>
                     <hr />
                 </div>
                 <div className="w3-container">
@@ -52,36 +128,7 @@ class RightContent extends Component {
                 </div>
             </div>
 
-            <div className="w3-container w3-card w3-white">
-                <h2 className="w3-text-grey w3-padding-16">
-                    <i className="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>
-                    Education
-                </h2>
-                <div className="w3-container">
-                    <h5 className="w3-opacity"><strong>W3Schools.com</strong></h5>
-                    <h6 className="w3-text-teal">
-                        <i className="fa fa-calendar fa-fw w3-margin-right"></i>Forever
-                    </h6>
-                    <p>Web Development! All I need to know in one place</p>
-                    <hr />
-                </div>
-                <div className="w3-container">
-                    <h5 className="w3-opacity"><strong>London Business School</strong></h5>
-                    <h6 className="w3-text-teal">
-                        <i className="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015
-                    </h6>
-                    <p>Master Degree</p>
-                    <hr />
-                </div>
-                <div className="w3-container">
-                    <h5 className="w3-opacity"><strong>School of Coding</strong></h5>
-                    <h6 className="w3-text-teal">
-                        <i className="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013
-                    </h6>
-                    <p>Bachelor Degree</p>
-                    <br />
-                </div>
-            </div>
+            
         </div>
     );
   }
