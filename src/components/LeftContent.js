c54889351f1b9f2bfde836e5b909b9353233f68b
VietNT <viet.nguyen@fujitechjsc.com>
2019-04-17 18:15:06 +0700

    update skills

--- src/components/LeftContent.js
+++ src/components/LeftContent.js
@@ -3,6 +3,95 @@ import Avatar from '../assets/img/avatar.png';
 
 class LeftContent extends Component {
   render() {
+    const skills = [
+        {
+            type: 'Web Programming',
+            items: [
+                {name: 'HTML5 / CSS3', value: 95},
+                {name: 'Nodejs / AngularJS / VueJS', value: 85},
+                {name: 'Javascript / Ajax', value: 85},
+                {name: 'Boostrap / Grid CSS', value: 80},
+                {name: 'Less / Sass', value: 80},
+                {name: 'JSON / JSONP / XML', value: 85},
+                {name: 'SEO', value: 65},
+                {name: 'Drupal', value: 95},
+                {name: 'Wordpress', value: 95},
+                {name: 'C#, .NET', value: 95},
+                {name: 'PHP', value: 95},
+                {name: 'MySQL / SQL', value: 95},
+                {name: 'Laravel, MVC, Yii2, FuelPHP', value: 95},
+                {name: 'Git, SVN, Gitlab', value: 80},
+                {name: 'Jenkin', value: 60},
+                {name: 'Ubuntu / CentOS', value: 80},
+                {name: 'Apache / Nginx', value: 80},
+                {name: 'Docker', value: 60},
+                {name: 'Markdown', value: 80},
+            ],
+        },{
+            type: 'Adobe Creative Cloud',
+            items: [
+                {name: 'Photoshop', value: 80},
+                {name: 'Illustrator', value: 50},
+                {name: 'InDesign', value: 70},
+                {name: 'Dreamweaver', value: 90},
+                {name: 'After Effects', value: 45},
+            ],
+        },{
+            type: 'Microsoft Office',
+            items: [
+                {name: 'Word', value: 90},
+                {name: 'Excel', value: 90},
+                {name: 'Powerpoint', value: 90},
+            ],
+        },{
+            type: 'Soft Skills',
+            items: [
+                {name: 'Communication', value: 95},
+                {name: 'Creativity and thinking', value: 90},
+                {name: 'Teamworking', value: 90},
+                {name: 'Making plan, schedule', value: 80},
+                {name: 'Honesty and trusty', value: 100},
+                {name: 'English', value: 80},
+                {name: 'Japanese', value: 40},
+            ],
+        },
+    ];
+    const hobbits = [
+        'Reading books',
+        'Playing football',
+        'Listening to music - Watching films',
+        'Photographing',
+        'Drinkng beer',
+    ];
+    const listSkills = skills.map((skill) => {
+        let items = skill.items;
+        let listItems = items.map((item) => {
+            return (
+                <div>
+                    <p>{item.name}</p>
+                    <div className="w3-light-grey w3-round-xlarge w3-small">
+                        <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:item.value + '%'}}>
+                            <div className="w3-center w3-text-white">{item.value}%</div>
+                        </div>
+                    </div>
+                </div>
+            )
+        });
+        return (
+            <div>
+                <p className="w3-large">
+                    <strong>
+                        <i className="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>
+                        {skill.type}
+                    </strong>
+                </p>
+                {listItems}
+                <br />
+            </div>
+        );
+    });
+    const listHobbits = hobbits.map((hobbit) => <li>{hobbit}</li>);
+
     return (
         <div className="w3-third">
             <div className="w3-white w3-text-grey w3-card-4">
@@ -41,44 +130,12 @@ class LeftContent extends Component {
                     </p>
                     <hr />
 
-                    <p className="w3-large">
-                        <strong><i className="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</strong>
-                    </p>
-                    <p>Adobe Photoshop</p>
-                    <div className="w3-light-grey w3-round-xlarge w3-small">
-                        <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:90 + '%'}}>90%</div>
-                    </div>
-                    <p>Photography</p>
-                    <div className="w3-light-grey w3-round-xlarge w3-small">
-                        <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:80 + '%'}}>
-                            <div className="w3-center w3-text-white">80%</div>
-                        </div>
-                    </div>
-                    <p>Illustrator</p>
-                    <div className="w3-light-grey w3-round-xlarge w3-small">
-                        <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:75 + '%'}}>75%</div>
-                    </div>
-                    <p>Media</p>
-                    <div className="w3-light-grey w3-round-xlarge w3-small">
-                        <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:50 + '%'}}>50%</div>
-                    </div>
-                    <br />
+                    {listSkills}
 
                     <p className="w3-large w3-text-theme">
-                        <strong><i className="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Languages</strong>
+                        <strong><i className="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Hobbits</strong>
                     </p>
-                    <p>English</p>
-                    <div className="w3-light-grey w3-round-xlarge">
-                        <div className="w3-round-xlarge w3-teal" style={{height:24, width:100 + '%'}}></div>
-                    </div>
-                    <p>Spanish</p>
-                    <div className="w3-light-grey w3-round-xlarge">
-                        <div className="w3-round-xlarge w3-teal" style={{height:24, width:55 + '%'}}></div>
-                    </div>
-                    <p>German</p>
-                    <div className="w3-light-grey w3-round-xlarge">
-                        <div className="w3-round-xlarge w3-teal" style={{height:24, width:25 + '%'}}></div>
-                    </div>
+                    <ul>{listHobbits}</ul>
                     <br />
 
                 </div>
