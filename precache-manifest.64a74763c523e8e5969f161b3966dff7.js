1ec76ffed89d6d93b25e315eeca99f4d23832a9c
Viet NT <tienvietnguyen1110@gmail.com>
2021-07-14 00:00:11 +0700

    Updates

--- precache-manifest.64a74763c523e8e5969f161b3966dff7.js
+++ precache-manifest.64a74763c523e8e5969f161b3966dff7.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "8876ff523821b169cc8a",
+    "revision": "2ab437321f320113e7ed",
     "url": "/VNT-Resume/static/css/main.16c3a85e.chunk.css"
   },
   {
-    "revision": "8876ff523821b169cc8a",
-    "url": "/VNT-Resume/static/js/main.dd85cf91.chunk.js"
+    "revision": "2ab437321f320113e7ed",
+    "url": "/VNT-Resume/static/js/main.418050b7.chunk.js"
   },
   {
     "revision": "47e6e91c31a763d8be44",
     "url": "/VNT-Resume/static/js/runtime~main.58477628.js"
   },
   {
-    "revision": "0154f9689bcca81074f4",
-    "url": "/VNT-Resume/static/js/2.be2527d6.chunk.js"
+    "revision": "848685eaea9f200be7ee",
+    "url": "/VNT-Resume/static/js/2.5ea26a04.chunk.js"
   },
   {
     "revision": "f516b3900b14127516e7ce6afbffda2c",
     "url": "/VNT-Resume/static/media/avatar.f516b390.png"
   },
   {
-    "revision": "1a408bb62ba24d79ee3b07ea4f7f4d5e",
+    "revision": "e139f9ba36e06c00a825de8d90dba684",
     "url": "/VNT-Resume/index.html"
   }
 ];
