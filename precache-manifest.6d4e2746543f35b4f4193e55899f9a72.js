e18609cc0614a0dd07b0654a46489c8519f0564b
VietNT <vietnt.working@gmail.com>
2024-02-01 19:09:57 +0700

    OK

---
+++ precache-manifest.6d4e2746543f35b4f4193e55899f9a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcf53737a20e6840537e",
+    "url": "/VNT-Resume/static/css/main.0aea132f.chunk.css"
+  },
+  {
+    "revision": "bcf53737a20e6840537e",
+    "url": "/VNT-Resume/static/js/main.14846bae.chunk.js"
+  },
+  {
+    "revision": "b2333e3369b8d9f77232",
+    "url": "/VNT-Resume/static/js/runtime~main.d33b36cd.js"
+  },
+  {
+    "revision": "a759d495ae3f66636dda",
+    "url": "/VNT-Resume/static/js/2.e36cbb6c.chunk.js"
+  },
+  {
+    "revision": "f516b3900b14127516e7ce6afbffda2c",
+    "url": "/VNT-Resume/static/media/avatar.f516b390.png"
+  },
+  {
+    "revision": "a157d75f5b70048e7b191e31b5ad000a",
+    "url": "/VNT-Resume/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/VNT-Resume/precache-manifest.bac548bb0824dc1803ee20a04f5eb3a8.js"
+  "/VNT-Resume/precache-manifest.6d4e2746543f35b4f4193e55899f9a72.js"
 );
 
 workbox.clientsClaim();
